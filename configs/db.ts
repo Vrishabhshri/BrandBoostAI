6f0fb9e39417d64e3aa76dfa08c529ff0c396e20
Daniel Hernandez <daniel_fhernandez@yahoo.com>
2025-02-21 21:22:12 -0800

    updated Drizzle files

--- configs/db.ts
+++ configs/db.ts
@@ -1,17 +1,5 @@
 import { drizzle } from 'drizzle-orm/neon-http';
-import { Pool } from "pg";
 
-const connectionString = process.env.NEXT_PUBLIC_DATABASE_CONNECTION_STRING;
-let pool;
+const db = drizzle(process.env.NEXT_PUBLIC_DATABASE_CONNECTION_STRING!);
 
-if (!connectionString) {
-  throw new Error("DATABASE_URL is not defined");
-}
-else {
-    pool = new Pool({
-        connectionString: connectionString,
-        ssl: true,
-    });
-}
-
-export const db = drizzle(pool);
+export { db };
