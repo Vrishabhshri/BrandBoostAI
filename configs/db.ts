d9a44aad1490e325961919b83bd3a46a301bfa10
Vrishabhshri <vrishabhshri@gmail.com>
2025-02-22 19:02:53 -0500

    Commented pool function out

--- configs/db.ts
+++ configs/db.ts
@@ -1,19 +1,19 @@
-"use server"
+// "use server"
 
 import { drizzle } from 'drizzle-orm/neon-http';
 import { Pool } from "pg";
 
-const connectionString = process.env.NEXT_PUBLIC_DATABASE_CONNECTION_STRING;
-let pool;
+// const connectionString = process.env.NEXT_PUBLIC_DATABASE_CONNECTION_STRING;
+// let pool;
 
-if (!connectionString) {
-  throw new Error("DATABASE_URL is not defined");
-}
-else {
-    pool = new Pool({
-        connectionString: connectionString,
-        ssl: true,
-    });
-}
+// if (!connectionString) {
+//   throw new Error("DATABASE_URL is not defined");
+// }
+// else {
+//     pool = new Pool({
+//         connectionString: connectionString,
+//         ssl: true,
+//     });
+// }
 
-export const db = drizzle(pool);
+// export const db = drizzle(pool);
