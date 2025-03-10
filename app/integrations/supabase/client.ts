22a17f5a119fec70002380720a73b7c22a666bd2
rorschach3 <daniel_fhernandez@yahoo.com>
2025-03-10 16:11:05 -0700

    updated Supabase database

--- app/integrations/supabase/client.ts
+++ app/integrations/supabase/client.ts
@@ -1,7 +1,7 @@
 
 
 const supabaseUrl = 'https://xnfpsjvlrfvfjdsidxrz.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhuZnBzanZscmZ2Zmpkc2lkeHJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEzNzg3OTQsImV4cCI6MjA1Njk1NDc5NH0.rmeZw5GPF1NpNsgzXw_MnMBj_asF2Xv2eKXV5kLbmbE'
 
 
 
--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Define constants for Supabase URL and key
-const supabaseUrl = 'https://xtnnkybxjewgzadcahzd.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh0bm5reWJ4amV3Z3phZGNhaHpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA2MTE0MzksImV4cCI6MjA1NjE4NzQzOX0.ZKZ6Rt9jsbiWNQn65z6YSgwiFm8nJH-s7kMnoUNL7LQ';
+const supabaseUrl = 'https://xnfpsjvlrfvfjdsidxrz.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhuZnBzanZscmZ2Zmpkc2lkeHJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEzNzg3OTQsImV4cCI6MjA1Njk1NDc5NH0.rmeZw5GPF1NpNsgzXw_MnMBj_asF2Xv2eKXV5kLbmbE';
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Missing Supabase configuration');
